4178a003dfa305a08a02ff5c502df5d8cda796a3
Evgeniy <paut.evg@yandex.ru>
2023-10-05 19:16:41 +0300

    feature(util): add jspHelper and urlPath

---
+++ src/main/java/by/javaguru/util/UrlPath.java
@@ -0,0 +1,8 @@
+package by.javaguru.util;
+
+public class UrlPath {
+    public static final String LOGIN = "/login";
+    public static final String LOGOUT = "/logout";
+    public static final String REGISTRATION = "/registration";
+    public static final String ACCOUNT = "/account";
+}
