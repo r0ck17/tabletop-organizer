5e929407724e0f058ff5690f3671b148d93a2920
Evgeniy <paut.evg@yandex.ru>
2023-10-05 20:33:30 +0300

    refactor(entity): replace methods with lombok

--- src/main/java/by/javaguru/entity/BoardGame.java
+++ src/main/java/by/javaguru/entity/BoardGame.java
@@ -1,7 +1,10 @@
 package by.javaguru.entity;
 
-import java.util.Objects;
+import lombok.Builder;
+import lombok.Data;
 
+@Data
+@Builder
 public class BoardGame {
     private Long id;
     private String name;
@@ -11,95 +14,4 @@ public class BoardGame {
     private String publisher;
     private short minPlayers;
     private short maxPlayers;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getPrice() {
-        return price;
-    }
-
-    public void setPrice(Integer price) {
-        this.price = price;
-    }
-
-    public Short getYear() {
-        return year;
-    }
-
-    public void setYear(Short year) {
-        this.year = year;
-    }
-
-    public String getLanguage() {
-        return language;
-    }
-
-    public void setLanguage(String language) {
-        this.language = language;
-    }
-
-    public String getPublisher() {
-        return publisher;
-    }
-
-    public void setPublisher(String publisher) {
-        this.publisher = publisher;
-    }
-
-    public short getMinPlayers() {
-        return minPlayers;
-    }
-
-    public void setMinPlayers(short minPlayers) {
-        this.minPlayers = minPlayers;
-    }
-
-    public short getMaxPlayers() {
-        return maxPlayers;
-    }
-
-    public void setMaxPlayers(short maxPlayers) {
-        this.maxPlayers = maxPlayers;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        BoardGame boardGame = (BoardGame) o;
-        return minPlayers == boardGame.minPlayers && maxPlayers == boardGame.maxPlayers && Objects.equals(id, boardGame.id) && Objects.equals(name, boardGame.name) && Objects.equals(price, boardGame.price) && Objects.equals(year, boardGame.year) && Objects.equals(language, boardGame.language) && Objects.equals(publisher, boardGame.publisher);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, price, year, language, publisher, minPlayers, maxPlayers);
-    }
-
-    @Override
-    public String toString() {
-        return "BoardGame{" +
-               "id=" + id +
-               ", name='" + name + '\'' +
-               ", price=" + price +
-               ", year=" + year +
-               ", language='" + language + '\'' +
-               ", publisher='" + publisher + '\'' +
-               ", minPlayers=" + minPlayers +
-               ", maxPlayers=" + maxPlayers +
-               '}';
-    }
 }
--- src/main/java/by/javaguru/entity/User.java
+++ src/main/java/by/javaguru/entity/User.java
@@ -1,85 +1,14 @@
 package by.javaguru.entity;
 
-import java.util.Objects;
+import lombok.Builder;
+import lombok.Data;
 
+@Data
+@Builder
 public class User {
     private Long id;
     private Integer roleId;
     private String name;
     private String login;
     private String password;
-
-    public User() {
-    }
-
-    public User(Integer roleId, String name, String login, String password) {
-        this.roleId = roleId;
-        this.name = name;
-        this.login = login;
-        this.password = password;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Integer getRoleId() {
-        return roleId;
-    }
-
-    public void setRoleId(Integer roleId) {
-        this.roleId = roleId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return Objects.equals(id, user.id) && Objects.equals(roleId, user.roleId) && Objects.equals(name, user.name) && Objects.equals(login, user.login) && Objects.equals(password, user.password);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, roleId, name, login, password);
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-               "id=" + id +
-               ", role_id=" + roleId +
-               ", name='" + name + '\'' +
-               ", login='" + login + '\'' +
-               ", password='" + password + '\'' +
-               '}';
-    }
 }
