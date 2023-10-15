5d1c145dbd8bd3c8ddc8fd731e5bb4845e65da3f
Evgeniy <paut.evg@yandex.ru>
2023-10-15 23:41:38 +0300

    feature: add dao exception

---
+++ src/main/java/by/javaguru/exception/DaoException.java
@@ -0,0 +1,11 @@
+package by.javaguru.exception;
+
+public class DaoException extends RuntimeException {
+    public DaoException(String message) {
+        super(message);
+    }
+
+    public DaoException(Throwable cause) {
+        super(cause);
+    }
+}
